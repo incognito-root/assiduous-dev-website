27463a26557c96b066946b735ad3d35522962f17
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2024-06-24 17:58:56 +0500

    Project Setup

---
+++ assiduous-dev-website/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+<div className="max-w-7xl w-full">
+       
+      {/*<Nav Section  />*/}
+      {/*<Hero Section />*/}
+      {/*<Section      />*/} 
+      {/*<Section      />*/}
+      {/*<Section      />*/}
+      {/*<Footer       />*/}
+        
+      </div>
+    </main>
+  );
+}
