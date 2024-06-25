5385b49765449a45969a3e2f1ddb4185476da0e9
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2024-06-25 13:16:13 +0500

    tailwind config updated, added tailwind colors.

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-
+import { Barlow } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+const barlow = Barlow({ subsets: ["latin"], weight:["100","200","300","400","500","600","700","800","900",],
+  variable:"--font-barlow"
+ });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={barlow.variable}>
       <ThemeProvider
             attribute="class"
             defaultTheme="dark"
