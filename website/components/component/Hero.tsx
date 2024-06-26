7addfa92c97b8067d389de124eafd01bc27d0be1
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2024-06-27 01:07:27 +0500

    Updated Hero Section
    OHHHHHHHHHHH MAAAAAAAAAAAAAAAAA GAWWWWWWWWWWWWWWWWD IT WORKED

--- website/components/component/Hero.tsx
+++ website/components/component/Hero.tsx
@@ -6,10 +6,10 @@ const Hero = () => {
       
     <div className="h-full w-full bg-[#1A1A1A] ">
     
-  <div className ="flex h-full flex-col items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('Grill.svg')" }}>
+  <div className ="flex h-full flex-col items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('Main.svg')" }}>
  
     <div className="relative flex flex-col items-center justify-center gap-8">
-    <img className=' absolute w-[100%] top-[60vh] sm:top-[43vh] md:top-[34vh] lg:top-[30vh] 2xl:top-[30vh]' src="Hero.png" alt="HeroImage" />
+    {/* <img className=' absolute w-[100%] top-[60vh] sm:top-[43vh] md:top-[34vh] lg:top-[30vh] 2xl:top-[30vh]' src="Hero.png" alt="HeroImage" /> */}
       
       <h1 className="z-10 text-center sm:text-5xl text-4xl sm:w-full w-[350px] font-semibold text-white">Empowering Your Vision with Assiduous Solutions</h1>
       <div className="z-10 flex p-5 h-[150px] w-[370px] sm:h-[74px] sm:w-[699px] items-center justify-center rounded-lg border border-[#262626] bg-[#2424249d]">
